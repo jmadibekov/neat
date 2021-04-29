48fd4fc07a7ee97b026e633e4d07b9f058ee857a
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-04-29 17:41:22 +0600

    reformat backend

--- back/todo/models.py
+++ back/todo/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField()
+    # description = models.TextField()
     completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
-
